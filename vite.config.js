cc27aa0c9d6a59955e08d0da6d2e3e5c10295ca3
RupmNath <128634103+RupmNath@users.noreply.github.com>
2025-03-01 21:37:38 +0530

    Fixed homepage and Vite base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/flower-paradise-nursery-e-com-/", // Add your repository name here
+  base: "/flower-paradise-nursery-e-com/"
 });
